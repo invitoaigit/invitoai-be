df0d8ef71a2088ac60415da4862f3c23ad78cd53
zainvice <amazwings@gmail.com>
2025-01-30 01:32:03 -0700

    Update v1.0: Sub domain routing

--- controllers/templateController.js
+++ controllers/templateController.js
@@ -24,8 +24,8 @@ exports.getTemplateById = async (req, res) => {
 // Create a new template
 exports.createTemplate = async (req, res) => {
     try {
-        const { name, image, type, category, price, discount, currency, youtubeLink, templateData } = req.body;
-        const newTemplate = new Template({ name, image, category, type, currency, youtubeLink, price, discount, templateData });
+        const { name, image, category, price, discount, currency, youtubeLink, templateData } = req.body;
+        const newTemplate = new Template({ name, image, category, currency, youtubeLink, price, discount, templateData });
         const savedTemplate = await newTemplate.save();
         res.status(201).json(savedTemplate);
     } catch (error) {
--- models/Template.js
+++ models/Template.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const templateSchema = new mongoose.Schema({
     name: { type: String, required: true },
     image: { type: String, required: true },
-    type: { type: String, required: true},
     category: { type: String, required: true},
     youtubeLink: { type: String},
     price: { type: String, required: true },
